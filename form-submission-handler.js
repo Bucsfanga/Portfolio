8c9a985208c14b9d16dfd2ffad661b80ab6cad4d
Donald Thatcher <43966071+Bucsfanga@users.noreply.github.com>
2020-07-26 05:15:40 -0400

    cleaned up

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -77,6 +77,9 @@
           var thankYouMessage = form.querySelector(".thankyou_message");
           if (thankYouMessage) {
             thankYouMessage.style.display = "block";
+              setTimeout(function(){
+                window.location="contact.html";
+              },3000);
           }
         }
     };
